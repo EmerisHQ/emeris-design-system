912d65d289f0321d8c159c163b09a52d11e240e2
Jesse Eilers <eitjuh@gmail.com>
2022-04-13 13:35:59 +0200

    removing stuff that's not needed, updating readme

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <h1>App is not used. Run "yarn run storybook" instead.</h1>
 </template>
 
 <style>
