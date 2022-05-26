778c6f016e3cac67a37f1d4dd80612e71f58fce7
Jesse Eilers <eitjuh@gmail.com>
2022-05-26 18:09:06 +0200

    trying to get standard button to work so we can get started reusing things

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import "virtual:windi.css";
+import '../src/assets/scss/index.scss';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -8,4 +9,22 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  themes: {
+    clearable: false,
+    list: [{
+      name: 'Light',
+      class: ['light'],
+      color: '#ffffff',
+      default: true
+    }, {
+      name: 'Dark',
+      // The class dark will be added to the body tag
+      class: ['dark'],
+      color: '#000000'
+    }]
+  }
+};
+
+export const globalTypes = {
+  darkMode: true,
 };
---
+++ src/components/Button.stories.js
@@ -0,0 +1,22 @@
+import Button from './Button.vue';
+
+export default {
+  component: Button,
+  title: 'Button',
+};
+
+const Template = (args) => ({
+  components: { Button },
+  setup() { return { args }; },
+  template: '<Button v-bind="args" />',
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  name: 'Default'
+};
+
+export const Link = Template.bind({});
+Link.args = { 
+  name: 'Link',
+};
